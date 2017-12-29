a70ee8f83fe8f39efb58fc41c7d293b9d46714ee
“marcinkostka <marcinkostka@gmail.com>
2017-12-29 11:28:59 +0100

    Wyswietlanie planszy

--- src/main/java/com/kodilla/Checkers/Board.java
+++ src/main/java/com/kodilla/Checkers/Board.java
@@ -19,4 +19,32 @@ public class Board {
     void setFigure(int x, int y, Figure f){
         rows.get(y).setFigure(x,f);
     }
+
+    @Override
+    public String toString(){
+        String board = "__|A |B |C |D |E |F |G |H |\n";
+        Figure figure = null;
+
+        for (int x=0; x<8; x++){
+            //draw 1-8 numbers
+            board = board + (x+1) + " |";
+
+            for (int y=0; y<8; y++){
+                figure = getFigure(x,y);
+                board = board + figure.getColor();
+
+                if(figure instanceof Pawn){
+                    board = board + "P";
+                } else if (figure instanceof Queen) {
+                    board = board + "Q";;
+                } else {
+                    board = board + " ";
+                }
+
+                board = board + "|";
+            }
+            board = board + "\n";
+        }
+        return board;
+    }
 }
--- src/main/java/com/kodilla/Checkers/Checkers.java
+++ src/main/java/com/kodilla/Checkers/Checkers.java
@@ -3,5 +3,12 @@ package com.kodilla.Checkers;
 public class Checkers {
     public static void main(String[] args) {
 
+        Board board = new Board();
+        board.setFigure(0,0, new Pawn("B"));
+        board.setFigure(1,1, new Pawn("B"));
+        board.setFigure(0,2, new Pawn("B"));
+        board.setFigure(5,4, new Queen("B"));
+
+        System.out.println(board);
     }
 }
