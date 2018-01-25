483929bf188e4312f0c53e324362d7bb19e8e97f
“marcinkostka <marcinkostka@gmail.com>
2018-01-25 17:11:25 +0100

    8.3 Obsługa wyjątku

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,11 +1,29 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
+import com.kodilla.exception.io.FileReaderWithoutHandling;
+import java.io.IOException;
 
 public class ExceptionModuleRunner {
 
     public  static void main(String args[]) {
+
+        /*FileReaderWithoutHandling fileReaderWithoutHandling = new FileReaderWithoutHandling();
+        try {
+            fileReaderWithoutHandling.readFile();
+        } catch (IOException e) {
+            System.out.println("Oh no! Something went wrong!" + e);
+        } finally {
+            System.out.println("I am gonna be here ... always!");
+        }*/
+
         FileReader fileReader = new FileReader();
-        fileReader.readFile();
+        try {
+            fileReader.readFile();
+        } catch (FileReaderException e) {
+            System.out.println("Problem while reading file!");
+        }
+
     }
 }
