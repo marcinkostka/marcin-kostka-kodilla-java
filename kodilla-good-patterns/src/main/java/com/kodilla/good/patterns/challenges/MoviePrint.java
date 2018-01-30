5abdb4a07d6bc834f8665a8fa8a9336928bea983
“marcinkostka <marcinkostka@gmail.com>
2018-01-30 22:07:38 +0100

    9.1 funkcyjna iteracja po tytułach

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MoviePrint.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MoviePrint.java
@@ -1,4 +1,12 @@
 package com.kodilla.good.patterns.challenges;
 
 public class MoviePrint {
+
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+        movieStore.getMovies().entrySet().stream()
+                .flatMap(entry -> entry.getValue().stream())
+                .map(n->n+"!")
+                .forEach(System.out::print);
+    }
 }
